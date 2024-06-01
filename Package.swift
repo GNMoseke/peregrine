5baca1c863729b3189c6a4992a3566a2d310651e
Garrett <gmoseke@protonmail.com>
2024-06-01 12:52:40 +0200

    package: add SwiftCommand dependency
    great package

--- Package.swift
+++ Package.swift
@@ -6,12 +6,17 @@ let package = Package(
     name: "peregrine",
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(
+            url: "https://github.com/Zollerboy1/SwiftCommand.git",
+            from: "1.2.0"
+        ), 
     ],
     targets: [
         .executableTarget(
             name: "peregrine",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "SwiftCommand", package: "SwiftCommand")
             ]
         ),
     ]

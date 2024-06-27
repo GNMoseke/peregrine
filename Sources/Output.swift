2b5b93b3036da2f96311c47a74e01858ca857721
Garrett <gmoseke@protonmail.com>
2024-06-27 08:13:57 +0200

    chore: add MPL header to all source

--- Sources/Output.swift
+++ Sources/Output.swift
@@ -1,3 +1,8 @@
+// Output.swift
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import ArgumentParser
 
 func print(_ str: String, terminator: String = "\n", _ color: TextColor) {
--- Sources/Peregrine.swift
+++ Sources/Peregrine.swift
@@ -1,3 +1,8 @@
+// Peregrine.swift
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import ArgumentParser
 import Foundation
 import Puppy
@@ -95,7 +100,6 @@ extension Peregrine {
                 let testResults = try await testRunner.runTests(tests: tests)
                 try testRunner.output(results: testResults)
             }
-
 
             // only cleanup on fully successful run
             try cleanupLogFile(logger: logger)

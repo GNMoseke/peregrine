b95fe927628d19b3fd875e63c8a7d3166c995a04
Garrett Moseke <gmosedev@proton.me>
2025-02-19 13:08:46 +0100

    fix: correct command version to 1.0.3

--- Sources/Peregrine.swift
+++ Sources/Peregrine.swift
@@ -21,7 +21,7 @@ struct Peregrine: AsyncParsableCommand {
         generally favorable to `swift test --filter fooTest` where applicable. peregrine is meant to help you find that
         `fooTest` is having issues in the first place.
         """,
-        version: "1.0.1",
+        version: "1.0.3",
         subcommands: [Run.self, CountTests.self],
         defaultSubcommand: Run.self
     )
